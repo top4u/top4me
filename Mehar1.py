4978d80792fe99cd8ec14df2e0937a0d155232c5
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 12:09:07 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,10 +1,12 @@
-print(TRED + TBOLD + " COPY ALL CONTENT FROM BOX BELOW")
-print(TRED + TBOLD + " OPEN NEW NOTEBOOK")
-print(TRED + TBOLD + " PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
 TRED =  '\033[31m' # Green Text
 TGREEN =  '\033[32m' # Green Text
 TBOLD =  '\033[1m' # BOLD Text
 TBACKRED =  '\033[41m' # BACKGROUND Text
+
+print(TRED + TBOLD + " COPY ALL CONTENT FROM BOX BELOW")
+print(TRED + TBOLD + " OPEN NEW NOTEBOOK")
+print(TRED + TBOLD + " PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
+
 
 print(TBACKRED + TBOLD + "---------------------Copy from Here to end-----------------------------------")
 
