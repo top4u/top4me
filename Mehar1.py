9cb4536aa3806978c8a0575260345af5874ed96e
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 11:43:05 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,5 +1,9 @@
-print("COPY ALL CONTENT FROM BOX BELOW")
-print("OPEN NEW NOTEBOOK AND PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
+* print("\033[1;31;40m Bright Red COPY ALL CONTENT FROM BOX BELOW/n")
+* print("\033[1;31;40m Bright Red OPEN NEW NOTEBOOK/n")
+* print("\033[1;31;40m Bright Red PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS/n")
+
+---------------------Copy from Here to end-----------------------------------
+
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/top4u/top4me/main/test.py', "test.txt")
 example1 = "test.txt"
