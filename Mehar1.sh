f589f9cb5e0c26aeb3e394d53b84c5e64e96ba90
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 10:30:57 +0500

    Update Mehar1.sh

--- Mehar1.sh
+++ Mehar1.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-wget https://raw.githubusercontent.com/top4u/top4me/main/test.txt
-chmod +x test.txt
-xdg-open test.txt
-cat test.txt
+wget /resources/data/test.txt https://raw.githubusercontent.com/top4u/top4me/main/test.txt &> /dev/null
+txt = "test.txt"
+file1 = open(txt, "r")
+filecontent = file1.read()
+print(filecontent)
