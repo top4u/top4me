ab0f0641d0063019cf46e3f0347b4c391a0badc3
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 11:34:08 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,13 +1,9 @@
+print("COPY ALL CONTENT FROM BOX BELOW")
+print("OPEN NEW NOTEBOOK AND PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/top4u/top4me/main/test.py', "test.txt")
 example1 = "test.txt"
 file1 = open(example1, "r")
 filecontent = file1.read()
 print(filecontent)
-
-
-
-print("COPY ALL CONTENT FROM BOX BELOW"/n)
-
-print("OPEN NEW NOTEBOOK AND PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS"/n)
 
