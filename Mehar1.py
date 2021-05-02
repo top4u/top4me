ffef960e6a4c2b0ad6dbd6354520e4170e1ac109
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 11:29:17 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -4,3 +4,10 @@ example1 = "test.txt"
 file1 = open(example1, "r")
 filecontent = file1.read()
 print(filecontent)
+
+
+
+print("COPY ALL CONTENT FROM BOX BELOW"/n)
+
+print("OPEN NEW NOTEBOOK AND PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS"/n)
+
