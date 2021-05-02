9173bfaee01c6b42b842f52334d379d16df6f073
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 11:54:00 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,8 +1,12 @@
-* print(" COPY ALL CONTENT FROM BOX BELOW")
-* print(" OPEN NEW NOTEBOOK")
-* print(" PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
+print(" COPY ALL CONTENT FROM BOX BELOW")
+print(" OPEN NEW NOTEBOOK")
+print(" PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
 
 print("---------------------Copy from Here to end-----------------------------------")
+pip install colorama
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'some red text')
 
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/top4u/top4me/main/test.py', "test.txt")
