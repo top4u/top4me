1063930f57897be3e18981ca70c7f978204fd436
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 12:07:38 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,12 +1,13 @@
-print(" COPY ALL CONTENT FROM BOX BELOW")
-print(" OPEN NEW NOTEBOOK")
-print(" PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
+print(TRED + TBOLD + " COPY ALL CONTENT FROM BOX BELOW")
+print(TRED + TBOLD + " OPEN NEW NOTEBOOK")
+print(TRED + TBOLD + " PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
+TRED =  '\033[31m' # Green Text
 TGREEN =  '\033[32m' # Green Text
+TBOLD =  '\033[1m' # BOLD Text
+TBACKRED =  '\033[41m' # BACKGROUND Text
 
-print(TGREEN + "---------------------Copy from Here to end-----------------------------------")
+print(TBACKRED + TBOLD + "---------------------Copy from Here to end-----------------------------------")
 
-
-print(Fore.RED + 'some red text')
 
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/top4u/top4me/main/test.py', "test.txt")
