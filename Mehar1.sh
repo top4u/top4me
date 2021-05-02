793b66b9c9e6101e220692710ce105b19b83faaf
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 08:22:58 +0500

    Update Mehar1.sh

--- Mehar1.sh
+++ Mehar1.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-wget https://raw.githubusercontent.com/top4u/top4me/test.py
+wget https://raw.githubusercontent.com/top4u/top4me/main/test.py
 chmod +x test.py
-./test.py
+python test.py
