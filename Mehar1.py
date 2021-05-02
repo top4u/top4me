0fccd007764708c280aa4f1fce290b62edbe0404
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 11:46:38 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -2,7 +2,7 @@
 * print(" OPEN NEW NOTEBOOK")
 * print(" PASTE COPIED CONTENT & FOLLOW INSTRUCTIONS")
 
-prin("---------------------Copy from Here to end-----------------------------------")
+print("---------------------Copy from Here to end-----------------------------------")
 
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/top4u/top4me/main/test.py', "test.txt")
