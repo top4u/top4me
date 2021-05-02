5451df6daa900b76772ee98f2ed6ff2d9dc68fc8
top4u <53888397+top4u@users.noreply.github.com>
2021-05-02 10:48:45 +0500

    Update Mehar1.py

--- Mehar1.py
+++ Mehar1.py
@@ -1,4 +1,7 @@
+import urllib
 url = https://raw.githubusercontent.com/top4u/top4me/main/test.txt &> /dev/null
+  f = urllib.urlopen(url)
+  print f.read()
 txt = "test.txt"
 file1 = open(txt, "r")
 filecontent = file1.read()
