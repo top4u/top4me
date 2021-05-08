5cd61a04d33d8592bc8a71d9237e42aa7638b713
top4u <53888397+top4u@users.noreply.github.com>
2021-05-08 13:29:31 +0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
 import pyperclip as content
 text1 = "this is content for copying"
-read = content.copy(text1)
-print(read + "copied content successfully")
+content.copy(text1)
+print(text1 + "copied content successfully")
