2631ec7bd04da6b5b5d301720d42232bc68203cf
top4u <53888397+top4u@users.noreply.github.com>
2021-05-08 12:14:20 +0500

    Update test.py

--- test.py
+++ test.py
@@ -280,9 +280,9 @@ def setupVNC(ngrok_region = None, check_gpu_available = True):
   print(msg)
 
 def apps():
-  ! wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  ! sudo dpkg --install google-chrome-stable_current_amd64.deb
-  ! sudo add-apt-repository ppa:obsproject/obs-studio -y
+  ! wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb /dev/null
+  ! sudo dpkg --install google-chrome-stable_current_amd64.deb /dev/null
+  ! sudo add-apt-repository ppa:obsproject/obs-studio -y  /dev/null
   clear_output()
   my_apt = _MyApt()
   my_apt.installPkg("obs-studio", "vlc",  "firefox", "xarchiver", "xfce4", "xfce4-goodies")
