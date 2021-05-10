89b98300f2f37f349f2f14718a1ee4e8d75672eb
top4u <53888397+top4u@users.noreply.github.com>
2021-05-10 09:01:06 +0500

    Update test.py

--- test.py
+++ test.py
@@ -283,10 +283,10 @@ def apps():
   print("Rdp Installing Started Please Be patience")
   ! wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &> /dev/null
   ! sudo dpkg --install google-chrome-stable_current_amd64.deb &> /dev/null
-  ! sudo add-apt-repository ppa:obsproject/obs-studio -y  &> /dev/null
+  ! sudo add-apt-repository ppa:obsproject/obs-studio -y 
   clear_output()
   my_apt = _MyApt()
-  my_apt.installPkg("obs-studio", "vlc",  "firefox", "xarchiver", "xfce4", "xfce4-goodies")
+  my_apt.installPkg("obs-studio", "firefox", "xfce4", "xfce4-goodies")
   my_apt.commit()
   clear_output()
 
